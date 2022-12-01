07bb127e833c99b1eaada053823176e778a8e2ac
Akhilesh Yadav1 <akhilesh_yadav1@epam.com>
2022-12-01 12:52:58 +0530

    Task 1 using NUnit

---
+++ MainClass.cs
@@ -0,0 +1,26 @@
+﻿using Chrome;
+using Edge;
+using Firefox;
+
+namespace Task_1___Using_Nunit_and_class_library
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            FirefoxTests firefox = new FirefoxTests();
+            firefox.Setup();
+            firefox.Test1();
+            firefox.EndTest();
+            EdgeTests edge = new EdgeTests();
+            edge.Setup();
+            edge.Test1();
+            edge.EndTest();
+            ChromeTests chrome = new ChromeTests();
+            chrome.Setup();
+            chrome.Test1();
+            chrome.EndTest();
+
+        }
+    }
+}
