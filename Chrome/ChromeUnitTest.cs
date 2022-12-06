7ab27a86ed5b8ce84fd204149223b96fe6a66f06
Akhilesh Yadav1 <akhilesh_yadav1@epam.com>
2022-12-06 16:51:35 +0530

    second commit

--- Chrome/ChromeUnitTest.cs
+++ Chrome/ChromeUnitTest.cs
@@ -18,25 +18,29 @@ namespace Chrome
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
 
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
 
             IWebElement username = driver.FindElement(By.XPath("//input[@id=\"user-name\"]"));
             username.SendKeys("standard_user");
             IWebElement password = driver.FindElement(By.XPath("//input[@id=\"password\"]"));
             password.SendKeys("secret_sauce");
             IWebElement loginButton = driver.FindElement(By.XPath("//input[@id=\"login-button\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             loginButton.Click();
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             String priceBefore = driver.FindElements(By.XPath("//div[@class=\"inventory_item_price\"]")).First().Text;
             String itemNameBefore = driver.FindElement(By.XPath("//div[contains(text(), \"Sauce Labs Backpack\")]")).Text;
 
+            IDictionary<string, string> itemsBeforeAddingToCart = new Dictionary<string, string>();
+            itemsBeforeAddingToCart.Add(itemNameBefore, priceBefore);
+
+
             IWebElement butronForAddToCart = driver.FindElement(By.XPath("//button[@id=\"add-to-cart-sauce-labs-backpack\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             butronForAddToCart.Click();
 
             IWebElement buttonForCart = driver.FindElement(By.XPath("//a[@class=\"shopping_cart_link\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             buttonForCart.Click();
 
             String priceAfter = driver.FindElement(By.XPath("//div[@class=\"inventory_item_price\"]")).Text;
@@ -44,7 +48,7 @@ namespace Chrome
             Assert.That(priceBefore, Is.EqualTo(priceAfter));
 
             IWebElement checkoutButton = driver.FindElement(By.XPath("//button[@id=\"checkout\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             checkoutButton.Click();
 
             IWebElement firstName = driver.FindElement(By.XPath("//input[@id=\"first-name\"]"));
@@ -54,7 +58,7 @@ namespace Chrome
             IWebElement postelCode = driver.FindElement(By.XPath("//input[@id=\"postal-code\"]"));
             postelCode.SendKeys("208007");
             IWebElement continueButton = driver.FindElement(By.XPath("//input[@id=\"continue\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             continueButton.Click();
 
             String itemNameAfterCheckout = driver.FindElement(By.XPath("//div[contains(text(), \"Sauce Labs Backpack\")]")).Text;
--- Edge/EdgeUnitTest.cs
+++ Edge/EdgeUnitTest.cs
@@ -18,25 +18,25 @@ namespace Edge
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
 
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
 
             IWebElement username = driver.FindElement(By.XPath("//input[@id=\"user-name\"]"));
             username.SendKeys("standard_user");
             IWebElement password = driver.FindElement(By.XPath("//input[@id=\"password\"]"));
             password.SendKeys("secret_sauce");
             IWebElement loginButton = driver.FindElement(By.XPath("//input[@id=\"login-button\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             loginButton.Click();
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             String priceBefore = driver.FindElements(By.XPath("//div[@class=\"inventory_item_price\"]")).First().Text;
             String itemNameBefore = driver.FindElement(By.XPath("//div[contains(text(), \"Sauce Labs Backpack\")]")).Text;
 
             IWebElement butronForAddToCart = driver.FindElement(By.XPath("//button[@id=\"add-to-cart-sauce-labs-backpack\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             butronForAddToCart.Click();
 
             IWebElement buttonForCart = driver.FindElement(By.XPath("//a[@class=\"shopping_cart_link\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             buttonForCart.Click();
 
             String priceAfter = driver.FindElement(By.XPath("//div[@class=\"inventory_item_price\"]")).Text;
@@ -44,7 +44,7 @@ namespace Edge
             Assert.That(priceBefore, Is.EqualTo(priceAfter));
 
             IWebElement checkoutButton = driver.FindElement(By.XPath("//button[@id=\"checkout\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             checkoutButton.Click();
 
             IWebElement firstName = driver.FindElement(By.XPath("//input[@id=\"first-name\"]"));
@@ -54,7 +54,7 @@ namespace Edge
             IWebElement postelCode = driver.FindElement(By.XPath("//input[@id=\"postal-code\"]"));
             postelCode.SendKeys("208007");
             IWebElement continueButton = driver.FindElement(By.XPath("//input[@id=\"continue\"]"));
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
             continueButton.Click();
 
             String itemNameAfterCheckout = driver.FindElement(By.XPath("//div[contains(text(), \"Sauce Labs Backpack\")]")).Text;
@@ -63,7 +63,7 @@ namespace Edge
             Assert.That(itemNameBefore, Is.EqualTo(itemNameAfterCheckout));
             Assert.That(priceBefore, Is.EqualTo(priceAfterCheckout));
 
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
         }
 
         [TearDown]
